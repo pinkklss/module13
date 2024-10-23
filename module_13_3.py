3533b7356050026ff45abcc2ebe0934a30d1a661
pinkklss <shaker_04@mail.ru>
2024-10-23 17:28:03 +0700

    Update module_13_3.py

--- module_13_3.py
+++ module_13_3.py
@@ -15,10 +15,7 @@ async def start(message):
 
 @dp.message_handler()
 async def all_message(message):
-    if message.text == '/start':
-        return
-    else:
-        await message.answer('Введите команду /start, чтобы начать общение.')
+    print('Введите команду /start, чтобы начать общение.')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
